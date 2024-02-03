0c6460f4894faaa86824f877414391e337d2c57f
Renangsa <rgsa@cin.ufpe.br>
2024-02-03 13:59:26 -0300

    Add:Player movement

--- Scenes/Game/Player/player.gd
+++ Scenes/Game/Player/player.gd
@@ -1,5 +1,14 @@
 extends CharacterBody2D
 
+@export var SPEED: int = 150
 
 func _ready():
 	$SpritePlayer/AnimationPlayer.play("idle")
+
+func get_input():
+	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity = input_direction * SPEED
+	
+func _physics_process(_delta):
+	get_input()
+	move_and_slide()
