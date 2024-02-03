1d16efc95055560c22ff319d3e077d921d42a456
Renangsa <rgsa@cin.ufpe.br>
2024-02-03 15:09:06 -0300

    Add: Animated sprites when player moves

--- Scenes/Game/Player/player.gd
+++ Scenes/Game/Player/player.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
 
 @export var SPEED: int = 150
+@onready var animations = $SpritePlayer/AnimationPlayer
+var look_direction = "down"
 
-func _ready():
-	$SpritePlayer/AnimationPlayer.play("idle")
+func update_animation():
+	if velocity.length() == 0:
+		animations.play("idle_" + look_direction)
+	else:
+		if velocity.x > 0: look_direction = "right"
+		elif velocity.x < 0: look_direction = "left"
+		elif velocity.y < 0 : look_direction = "up"
+		elif velocity.y > 0 : look_direction = "down"
+		animations.play("walking_" + look_direction)
+	
 
 func get_input():
-	var input_direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var input_direction = Input.get_vector("LEFT","RIGHT","UP","DOWN")
 	velocity = input_direction * SPEED
+
 	
 func _physics_process(_delta):
 	get_input()
 	move_and_slide()
+	update_animation()
