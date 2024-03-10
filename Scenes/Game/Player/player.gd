5e3007b49c461ced4a5fe7e61873ffc76589821c
Renangsa <rgsa@cin.ufpe.br>
2024-03-10 13:57:49 -0300

    Add: Adição do Sprite inicial do mapa com as 3 áreas / Outras pequenas mudanças

--- Scenes/Game/Player/player.gd
+++ Scenes/Game/Player/player.gd
@@ -1,18 +1,18 @@
 extends CharacterBody2D
 
 @export var SPEED: int = 150
-@onready var animations = $SpritePlayer/AnimationPlayer
+@onready var player_animation = $SpritePlayer/AnimationPlayer
 var look_direction = "down"
 
 func update_animation():
 	if velocity.length() == 0:
-		animations.play("idle_" + look_direction)
+		player_animation.play("idle_" + look_direction)
 	else:
 		if velocity.x > 0: look_direction = "right"
 		elif velocity.x < 0: look_direction = "left"
 		elif velocity.y < 0 : look_direction = "up"
 		elif velocity.y > 0 : look_direction = "down"
-		animations.play("walking_" + look_direction)
+		player_animation.play("walking_" + look_direction)
 	
 
 func get_input():
